235bc810495884bceaecfd375ddcd55f7cfc55e6
Łukasz Olszewski <dev@lukaszolszewski.info>
2024-01-06 14:18:19 +0100

    no auth

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,12 +2,9 @@ import 'package:auto_gpt_flutter_client/services/leaderboard_service.dart';
 import 'package:auto_gpt_flutter_client/services/shared_preferences_service.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/settings_viewmodel.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/task_queue_viewmodel.dart';
-import 'package:auto_gpt_flutter_client/views/auth/firebase_auth_view.dart';
 import 'package:flutter/material.dart';
 import 'views/main_layout.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:auto_gpt_flutter_client/viewmodels/task_viewmodel.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/chat_viewmodel.dart';
@@ -22,17 +19,6 @@ import 'package:auto_gpt_flutter_client/utils/rest_api_utility.dart';
 // TODO: Update documentation throughout project for consistency
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyBvYLAK_A0uhFuVPQbTxUdVWbb_Lsur9cg',
-      authDomain: 'prod-auto-gpt.firebaseapp.com',
-      projectId: 'prod-auto-gpt',
-      storageBucket: 'prod-auto-gpt.appspot.com',
-      messagingSenderId: '387936576242',
-      appId: '1:387936576242:web:7536e0c50dd81b4dd7a66b',
-      measurementId: 'G-8PRS69JJRL',
-    ),
-  );
 
   runApp(
     MultiProvider(
@@ -86,18 +72,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          }
-          String hostname = Uri.base.host;
-
-          if (snapshot.hasData && snapshot.data != null ||
-              hostname.contains('github.dev')) {
-            return MultiProvider(
-              providers: [
+      home: MultiProvider(
+        providers: [
                 ChangeNotifierProvider(
                   create: (context) => ChatViewModel(
                     Provider.of<ChatService>(context, listen: false),
@@ -123,11 +99,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ],
-              child: MainLayout(),
-            ); // User is signed in
-          }
-          return FirebaseAuthView(); // User is not signed in, show auth UI
-        },
+        child: MainLayout(),
       ),
     );
   }
