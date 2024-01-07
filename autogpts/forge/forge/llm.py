2a7aff85c64987e857d1fd4e3ff7f5c4ea1712be
Łukasz Olszewski <dev@lukaszolszewski.info>
2024-01-07 11:18:00 +0100

    updating forge to openai api v1

--- autogpts/forge/forge/llm.py
+++ autogpts/forge/forge/llm.py
@@ -1,6 +1,9 @@
 import typing
 
-import openai
+from openai import OpenAI, AsyncOpenAI
+
+client = OpenAI()
+aclient = AsyncOpenAI()
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 
 from .sdk.forge_log import ForgeLogger
@@ -36,10 +39,8 @@ async def create_embedding_request(
 ) -> typing.Union[typing.Dict[str, typing.Any], Exception]:
     """Generate an embedding for a list of messages using OpenAI's API"""
     try:
-        return await openai.Embedding.acreate(
-            input=[f"{m['role']}: {m['content']}" for m in messages],
-            engine=model,
-        )
+        return await aclient.embeddings.create(input=[f"{m['role']}: {m['content']}" for m in messages],
+        engine=model)
     except Exception as e:
         LOG.error("Unable to generate ChatCompletion response")
         LOG.error(f"Exception: {e}")
@@ -52,7 +53,7 @@ async def transcribe_audio(
 ) -> typing.Union[typing.Dict[str, typing.Any], Exception]:
     """Transcribe an audio file using OpenAI's API"""
     try:
-        return await openai.Audio.transcribe(model="whisper-1", file=audio_file)
+        return await client.audio.transcribe(model="whisper-1", file=audio_file)
     except Exception as e:
         LOG.error("Unable to generate ChatCompletion response")
         LOG.error(f"Exception: {e}")
